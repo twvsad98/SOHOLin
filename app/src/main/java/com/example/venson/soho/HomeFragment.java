571c7cf02fbf0c6a2113cc39ca6167d1c04ceebb
twvsad98 <twvsad98@gmail.com>
2018-04-21 22:52:22 +0800

    個人資料頁面

---
+++ app/src/main/java/com/example/venson/soho/HomeFragment.java
@@ -0,0 +1,21 @@
+package com.example.venson.soho;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ricky on 2018/4/21.
+ */
+
+public class HomeFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/venson/soho/PersonActivity.java
@@ -0,0 +1,81 @@
+package com.example.venson.soho;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class PersonActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.cag_item_Home:
+                    fragment = new HomeFragment();
+                    switchFragment(fragment);
+                    setTitle(R.string.cag_nav_home);
+                    return true;
+                case R.id.cag_item_Mail:
+                    fragment = new MessageFragment();
+                    switchFragment(fragment);
+                    setTitle(R.string.cag_nav_message);
+                    return true;
+                case R.id.cag_item_Case:
+                    fragment = new CaseFragment();
+                    switchFragment(fragment);
+                    setTitle(R.string.cag_nav_case);
+                    return true;
+                case R.id.cag_item_Member:
+                    fragment = new UserFragment();
+                    switchFragment(fragment);
+                    setTitle(R.string.cag_nav_member);
+                    return true;
+                case R.id.cag_item_Settimg:
+                    fragment = new SettingsFragment();
+                    switchFragment(fragment);
+                    setTitle(R.string.cag_nav_settings);
+                    return true;
+                default:
+                    initContent();
+                    break;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.person_layout);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.cag_bottomNavigation);
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        initContent();
+    }
+
+    private void initContent() {
+        Fragment fragment = new HomeFragment();
+        switchFragment(fragment);
+        setTitle(R.string.cag_nav_home);
+    }
+
+    private void switchFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.content, fragment);
+        fragmentTransaction.commit();
+    }
+
+    }
+
