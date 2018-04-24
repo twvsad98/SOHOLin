192ffb9241065f471ca9c94c94bba1a168bbecb0
venson <hiddleston0910@gmail.com>
2018-04-24 15:09:20 +0800

    update 02

---
+++ app/src/main/java/com/example/venson/soho/Member/MemberFragment.java
@@ -0,0 +1,21 @@
+package com.example.venson.soho.Member;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.venson.soho.R;
+
+
+public class MemberFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.member_layout, container, false);
+        return view;
+    }
+
+}
